131962470711fe488a8e92a3dff441f9f6d0ffaf
Andreas Linden <zlx@gmx.de>
2024-01-29 17:14:32 +0100

    fix namespace for symfony configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Zolex\VomBundle\DependencyInjection;
+namespace Zolex\VOM\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
