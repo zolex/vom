7d7ac064cf755ce43fc38a0a11500deeb978d022
Andreas Linden <andreas.linden@coxautoinc.com>
2024-01-31 12:55:18 +0100

    Chore: remove unused imports.

--- tests/VersatileObjectMapper/CachedVersatileObjectMapperTest.php
+++ tests/VersatileObjectMapper/CachedVersatileObjectMapperTest.php
@@ -2,22 +2,11 @@
 
 namespace Zolex\VOM\Test\VersatileObjectMapper;
 
-use PHPUnit\Framework\TestCase;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Zolex\VOM\Metadata\Factory\CachedModelMetadataFactory;
 use Zolex\VOM\Metadata\Factory\ModelMetadataFactory;
 use Zolex\VOM\Metadata\Factory\PropertyMetadataFactory;
-use Zolex\VOM\Test\Fixtures\Address;
-use Zolex\VOM\Test\Fixtures\Arrays;
-use Zolex\VOM\Test\Fixtures\Booleans;
-use Zolex\VOM\Test\Fixtures\DateAndTime;
-use Zolex\VOM\Test\Fixtures\FlagParent;
-use Zolex\VOM\Test\Fixtures\Person;
-use Zolex\VOM\Test\Fixtures\SickChild;
-use Zolex\VOM\Test\Fixtures\SickRoot;
-use Zolex\VOM\Test\Fixtures\SickSack;
-use Zolex\VOM\Test\Fixtures\SickSuck;
 use Zolex\VOM\VersatileObjectMapper;
 
 class CachedVersatileObjectMapperTest extends VersatileObjectMapperTest
