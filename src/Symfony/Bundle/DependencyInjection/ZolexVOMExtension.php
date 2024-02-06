4f4762221f8794b2e5be6e072ba642a82e192683
Andreas Linden <zlx@gmx.de>
2024-02-06 20:17:02 +0100

    Fix: Remove old service definition and test for GroupsContextBuilder

--- src/Symfony/Bundle/DependencyInjection/ZolexVOMExtension.php
+++ src/Symfony/Bundle/DependencyInjection/ZolexVOMExtension.php
@@ -16,11 +16,8 @@ namespace Zolex\VOM\Symfony\Bundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Zolex\VOM\ApiPlatform\Serializer\GroupsContextBuilder;
 
 class ZolexVOMExtension extends Extension implements CompilerPassInterface
 {
@@ -42,13 +39,5 @@ class ZolexVOMExtension extends Extension implements CompilerPassInterface
 
     public function process(ContainerBuilder $container): void
     {
-        if ($container->has('api_platform.serializer.context_builder')) {
-            $groupsContextBuilder = new Definition(GroupsContextBuilder::class);
-            $groupsContextBuilder->setDecoratedService('api_platform.serializer.context_builder', null, -10);
-            $groupsContextBuilder->addArgument(new Reference('zolex_vom.serializer.groups_context_builder.inner'));
-            $groupsContextBuilder->addArgument(new Reference('zolex_vom.metadata.model_metadata_factory'));
-            $groupsContextBuilder->addArgument('%zolex_vom.serializer.groups_query_param%');
-            $container->setDefinition('zolex_vom.serializer.groups_context_builder', $groupsContextBuilder);
-        }
     }
 }
