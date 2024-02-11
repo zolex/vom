6c496e474a2ef79de1054c0ea3c1d5edc6e9ee2f
Andreas Linden <zlx@gmx.de>
2024-02-11 19:26:00 +0100

    Tests: Add a TestKernel and a basic test for the Symfony Bundle to assert that the essential services are registered and that VOM integrates with Symfony's serializer.

---
+++ tests/Symfony/Bundle/ZolexVOMBundleTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the VOM package.
+ *
+ * (c) Andreas Linden <zlx@gmx.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zolex\VOM\Test\Symfony\Bundle;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zolex\VOM\Test\Fixtures\Person;
+use Zolex\VOM\Test\Fixtures\Thing;
+
+class ZolexVOMBundleTest extends KernelTestCase
+{
+    protected static function getKernelClass(): string
+    {
+        return ZolexVOMTestKernel::class;
+    }
+
+    public function testServicesAreRegistered(): void
+    {
+        $kernel = self::bootKernel();
+        $container = $kernel->getContainer();
+
+        foreach (ZolexVOMTestKernel::SERVICES as $id => $class) {
+            $this->assertTrue($container->has($id));
+            $service = $container->get($id);
+            $this->assertInstanceOf($class, $service);
+        }
+    }
+
+    public function testNonDebugServicesAreRegistered(): void
+    {
+        $kernel = self::bootKernel(['environment' => 'prod', 'debug' => false]);
+        $container = $kernel->getContainer();
+
+        foreach (ZolexVOMTestKernel::NON_DEBUG_SERVICES as $id => $class) {
+            $this->assertTrue($container->has($id));
+            $service = $container->get($id);
+            $this->assertInstanceOf($class, $service);
+        }
+    }
+
+    public function testVomIntegratesWithSerializer(): void
+    {
+        $kernel = self::bootKernel();
+        $container = $kernel->getContainer();
+
+        $serializer = $container->get('serializer');
+        $data = [
+            'type' => 'person',
+            'name' => [
+                'firstname' => 'Peter',
+                'lastname' => 'Parker',
+            ],
+        ];
+
+        $person = $serializer->denormalize($data, Thing::class, null, ['vom' => true]);
+
+        $this->assertInstanceOf(Person::class, $person);
+        $this->assertEquals('Peter', $person->firstname);
+        $this->assertEquals('Parker', $person->lastname);
+    }
+}
---
+++ tests/Symfony/Bundle/ZolexVOMTestKernel.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the VOM package.
+ *
+ * (c) Andreas Linden <zlx@gmx.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zolex\VOM\Test\Symfony\Bundle;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Serializer\SerializerInterface;
+use Zolex\VOM\Metadata\Factory\ModelMetadataFactoryInterface;
+use Zolex\VOM\PropertyInfo\Extractor\MethodCallExtractor;
+use Zolex\VOM\Serializer\Normalizer\BooleanNormalizer;
+use Zolex\VOM\Serializer\Normalizer\CommonFlagNormalizer;
+use Zolex\VOM\Serializer\Normalizer\ObjectNormalizer;
+use Zolex\VOM\Serializer\VersatileObjectMapper;
+use Zolex\VOM\Symfony\Bundle\ZolexVOMBundle;
+
+class ZolexVOMTestKernel extends Kernel implements CompilerPassInterface
+{
+    use MicroKernelTrait;
+
+    public const SERVICES = [
+        'zolex_vom.metadata.model_metadata_factory' => ModelMetadataFactoryInterface::class,
+        'zolex_vom.serializer.versatile_object_mapper' => VersatileObjectMapper::class,
+        'zolex_vom.serializer.normalizer.object_normalizer' => ObjectNormalizer::class,
+        'zolex_vom.serializer.normalizer.boolean_normalizer' => BooleanNormalizer::class,
+        'zolex_vom.serializer.normalizer.common_flag_normalizer' => CommonFlagNormalizer::class,
+        'zolex_vom.property_info.method_call_extractor' => MethodCallExtractor::class,
+        'serializer' => SerializerInterface::class,
+    ];
+
+    public const NON_DEBUG_SERVICES = [
+        'zolex_vom.metadata.model_metadata_factory.cached' => ModelMetadataFactoryInterface::class,
+    ];
+
+    /**
+     * make services public so we can test them.
+     */
+    public function process(ContainerBuilder $container): void
+    {
+        foreach ($container->getDefinitions() as $id => $definition) {
+            if (\array_key_exists($id, self::SERVICES) || \array_key_exists($id, self::NON_DEBUG_SERVICES)) {
+                $definition->setPublic(true);
+            }
+        }
+    }
+
+    public function getVarDir(): string
+    {
+        return sys_get_temp_dir().'/zolex-vom-bundle/var';
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getVarDir().'/cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getVarDir().'/log';
+    }
+
+    public function getProjectDir(): string
+    {
+        return realpath('./tests/Symfony/Bundle');
+    }
+
+    public function registerBundles(): iterable
+    {
+        yield new FrameworkBundle();
+        yield new ZolexVOMBundle();
+    }
+
+    public function shutdown(): void
+    {
+        parent::shutdown();
+
+        $filesystem = new Filesystem();
+        $varDir = $this->getVarDir();
+        if ($filesystem->exists($varDir)) {
+            $filesystem->remove($varDir);
+        }
+    }
+}
