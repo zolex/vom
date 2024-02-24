e36cd1fb5f58f91f98e319b5838026be5fa1acd0
Andreas Linden <andreas.linden@coxautoinc.com>
2024-02-24 08:59:35 +0100

    Chore: Add a Laravel DummyApplication and DummyServiceProvider, so we can test the Laravel Service Provider without installing the illuminate dependencies (which are incompatible with symfony 7 and break our CI pipeline)

---
+++ tests/Laravel/Illuminate/Support/DummyServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the VOM package.
+ *
+ * (c) Andreas Linden <zlx@gmx.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zolex\VOM\Test\Laravel\Illuminate\Support;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class DummyServiceProvider
+{
+    public Application $app;
+}
