375b3279aacf43a3a6cd5dade5a79d2eaa4b4ec2
Andreas Linden <andreas.linden@coxautoinc.com>
2024-02-24 09:16:28 +0100

    Test: Add a test to cover the decorator exceptions in VersatileObjectMapper.

---
+++ tests/Serializer/DummyNormalizer.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the VOM package.
+ *
+ * (c) Andreas Linden <zlx@gmx.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zolex\VOM\Test\Serializer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class DummyNormalizer extends DummySerializer implements NormalizerInterface
+{
+    public function normalize(mixed $object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    {
+        return null;
+    }
+
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
+    {
+        return false;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [];
+    }
+}
